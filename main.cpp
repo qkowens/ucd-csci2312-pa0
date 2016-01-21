6f1a36ffa1e2e5c54a389ff74f719432bcf399b7
qkowens <quinn.owens@ucdenver.edu>
2016-01-20 21:52:56 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 /**
  * File: main.cpp
  * Project: ucd-csci2312-pa0
- * Author: Ivo Georgiev
+ * Original Author: Ivo Georgiev
+ * New Author: Quinn Owens
  * Date: 2016-01-18
  */
 #include <iostream>
 
 int main() {
-    // TODO Substitute 11 with your UCD ID number, modulo 13 (myId % 13)
-    std::cout << "Hello, world! My number is " << 23 << '!' << std::endl;
+    std::cout << "Hello, world! My number is " << 104 << '!' << std::endl;
 
     return 0;
 }
